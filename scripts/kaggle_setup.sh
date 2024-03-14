d57247ff4a4c5ad5dab9adf8cdfecfcb57ca912e
Trinq2003 <tri.nq210860@sis.hust.edu.vn>
2024-03-14 19:55:14 +0700

    kaggle setup scripts, package versions removed

--- scripts/kaggle_setup.sh
+++ scripts/kaggle_setup.sh
@@ -1,9 +1,9 @@
-pip install tqdm==4.58.0
-pip install requests==2.25.1
-pip install importlib-metadata==3.7.0
-pip install filelock==3.0.12
-pip install scikit-learn==0.24.1
-pip install tokenizers==0.10.1
-pip install explainaboard_client==0.0.7
+pip install tqdm
+pip install requests
+pip install importlib-metadata
+pip install filelock
+pip install scikit-learn
+pip install tokenizers
+pip install explainaboard_client
 pip install numpy
 pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
